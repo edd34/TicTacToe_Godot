a2988122c651e284783ebdb02848cb99d0b95c0e
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-04-12 00:01:33 +0200

    chhange text for About section

--- Scripts/Menu/About_Text.gd
+++ Scripts/Menu/About_Text.gd
@@ -7,15 +7,17 @@ onready var checkButton = get_node("../../Footer/CheckButton")
 func _ready():
 	line = 0
 	push_align(ALIGN_CENTER)
-	autoscroll_state = true
-	scroll_active = false
+	autoscroll_state = false
+	scroll_active = true
 	pass
 
 
 func _on_Timer_timeout():
 	if  autoscroll_state == true and line < get_line_count():
 		line+=1
-		scroll_to_line(line)		
+		scroll_to_line(line)
+		if line == get_line_count():
+			line = 0
 	pass # replace with function body
 
 
