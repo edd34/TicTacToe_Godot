0866d1fd089b7cf622e32986dc49dfcc03ba423d
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-05-11 23:23:20 +0200

    attach plus and minus pressed signal to method, and implement _on_X_press() to increment or decrement digit

--- Scripts/Network/digit_selector.gd
+++ Scripts/Network/digit_selector.gd
@@ -1,11 +1,20 @@
 extends VBoxContainer
 
-onready var Digit = $Label
+onready var myDigit = $Digit
+onready var plus = $Plus
+onready var minus = $Minus
 
 func _ready():
+	print(myDigit.name)
+	myDigit.text = str(0)
+	plus.connect("pressed",self,"_on_Plus_pressed")
+	minus.connect("pressed",self,"_on_Minus_pressed")
 	pass
 
 func _on_Plus_pressed():
-	print("ok")
+	myDigit.text = str(int(myDigit.text)+1)
 	pass
 
+func _on_Minus_pressed():
+	myDigit.text = str(int(myDigit.text)-1)
+	pass
