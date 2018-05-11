7aaecd5a37e9cb605f50c73a6cfbfb2fbed181c0
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-05-12 00:31:57 +0200

    implement behavior related to +/-10 or +/-100

--- Scripts/Network/digit_selector.gd
+++ Scripts/Network/digit_selector.gd
@@ -1,14 +1,28 @@
 extends VBoxContainer
 
 onready var myDigit = $Digit
+
 onready var plus = $Plus
 onready var minus = $Minus
+
+onready var plus10 = $Plus10
+onready var minus10 = $Minus10
+
+onready var plus100 = $Plus100
+onready var minus100 = $Minus100
 
 func _ready():
 	print(myDigit.name)
 	myDigit.text = str(255)
+
 	plus.connect("pressed",self,"_on_Plus_pressed")
 	minus.connect("pressed",self,"_on_Minus_pressed")
+
+	plus10.connect("pressed",self,"_on_Plus10_pressed")
+	minus10.connect("pressed",self,"_on_Minus10_pressed")
+
+	plus100.connect("pressed",self,"_on_Plus100_pressed")
+	minus100.connect("pressed",self,"_on_Minus100_pressed")
 	pass
 
 func _on_Plus_pressed():
@@ -19,4 +33,24 @@ func _on_Plus_pressed():
 func _on_Minus_pressed():
 	if int(myDigit.text) > 0:
 		myDigit.text = str(int(myDigit.text)-1)
+	pass
+
+func _on_Plus10_pressed():
+	if int(myDigit.text) <= 245:
+		myDigit.text = str(int(myDigit.text)+10)
+	pass
+
+func _on_Minus10_pressed():
+	if int(myDigit.text) >= 10:
+		myDigit.text = str(int(myDigit.text)-10)
+	pass
+
+func _on_Plus100_pressed():
+	if int(myDigit.text) <= 155:
+		myDigit.text = str(int(myDigit.text)+100)
+	pass
+
+func _on_Minus100_pressed():
+	if int(myDigit.text) >= 100:
+		myDigit.text = str(int(myDigit.text)-100)
 	pass
