e2880dc69ee6e3442c37a4b8a5c544cba1695555
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-05-11 23:30:56 +0200

    clamp value between 0 and 255

--- Scripts/Network/digit_selector.gd
+++ Scripts/Network/digit_selector.gd
@@ -6,15 +6,17 @@ onready var minus = $Minus
 
 func _ready():
 	print(myDigit.name)
-	myDigit.text = str(0)
+	myDigit.text = str(255)
 	plus.connect("pressed",self,"_on_Plus_pressed")
 	minus.connect("pressed",self,"_on_Minus_pressed")
 	pass
 
 func _on_Plus_pressed():
-	myDigit.text = str(int(myDigit.text)+1)
+	if int(myDigit.text) < 255:
+		myDigit.text = str(int(myDigit.text)+1)
 	pass
 
 func _on_Minus_pressed():
-	myDigit.text = str(int(myDigit.text)-1)
+	if int(myDigit.text) > 0:
+		myDigit.text = str(int(myDigit.text)-1)
 	pass
