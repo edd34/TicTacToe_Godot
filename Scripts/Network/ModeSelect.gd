cb2eb5098b636d80744cabb4ac5e4d7d6d02e0a0
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-06-18 22:00:46 +0200

    declare variable

--- Scripts/Network/ModeSelect.gd
+++ Scripts/Network/ModeSelect.gd
@@ -1,8 +1,9 @@
 extends OptionButton
 
 onready var selector = get_node(".")
-
-
+onready var clientModeNode = get_node("../ClientMode")
+onready var serverModeNode = get_node("../ServerMode")
+var mode
 
 func _ready():
 	selector.connect("item_selected",self,"_on_Plus_pressed")
