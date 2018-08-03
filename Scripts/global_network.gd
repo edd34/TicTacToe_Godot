978f7112d3cc8421e0f2de0cd335b078f55d29f5
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-08-03 21:50:49 +0200

    add setter and getter for IP address

--- Scripts/global_network.gd
+++ Scripts/global_network.gd
@@ -1,15 +1,16 @@
 extends Node
 
-var IP_address
-var network_connected = false
+var IP_address = ""
+#var network_connected = false
+enum network_status {client = 1, server = 2, not_connected = 0}
 
 func _ready():
 	pass
 
 func set_IP_address(param):
-	IP_address = param
+	IP_address = str(param)
 	pass
 
 func get_IP_address():
-	return IP_address
+	return str(IP_address)
 	pass
