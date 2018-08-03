3decbc60920f78ebcd5d03802337b713beda1966
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-08-03 21:46:01 +0200

    add relevant code to allow get IP printed

--- Scripts/Network/Client_GetInputIp.gd
+++ Scripts/Network/Client_GetInputIp.gd
@@ -4,15 +4,16 @@ onready var InputDigit1 = get_tree().get_root().get_node("Node/MarginContainer/M
 onready var InputDigit2 = get_tree().get_root().get_node("Node/MarginContainer/MainScreen/ClientMode/HBoxContainer/Part2/LineEdit2")
 onready var InputDigit3 = get_tree().get_root().get_node("Node/MarginContainer/MainScreen/ClientMode/HBoxContainer/Part3/LineEdit3")
 onready var InputDigit4 = get_tree().get_root().get_node("Node/MarginContainer/MainScreen/ClientMode/HBoxContainer/Part4/LineEdit4")
-onready var next_button = get_tree().get_root().get_node("Node/MarginContainer/MainScreen/Bottom/NextButton")
+onready var configure_button = get_tree().get_root().get_node("Node/MarginContainer/MainScreen/ClientMode/configure_btn")
 
-var input_IP_address
+var input_IP_address = ""
 
 func _ready():
-	next_button.connect("pressed",self,"_on_next_button_pressed")
+	configure_button.connect("pressed",self,"_on_conf_btn_pressed")
 	pass
 
-func _on_next_button_pressed():
+func _on_conf_btn_pressed():
+	print("conf btn pressed")
 	saveIpv4()
 	pass
 
@@ -28,13 +29,15 @@ func throwErrorDigit(digit):
 
 func getStringDigit():
 	input_IP_address = InputDigit1.text+"."+InputDigit2.text+"."+InputDigit3.text+"."+InputDigit4.text
-	return input_IP_address
+	print(input_IP_address)
+	return str(input_IP_address)
 	pass
 
 func getIpv4():
-	return get_node("/root/global_network").get_IP_address()
+	return str(get_node("/root/global_network").get_IP_address())
 	pass
 
 func saveIpv4():
-	get_node("/root/global_network").set_IP_address(input_IP_address)
+	get_node("/root/global_network").set_IP_address(getStringDigit())
+	print("IP address configured : " + getIpv4())
 	pass
