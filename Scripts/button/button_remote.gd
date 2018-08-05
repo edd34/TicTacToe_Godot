493f0240091a7801113ac1ac58b608ef2ded72a5
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-08-05 21:56:21 +0200

    add code that intialize player's pattern

--- Scripts/button/button_remote.gd
+++ Scripts/button/button_remote.gd
@@ -4,7 +4,20 @@ onready var board = get_tree().get_root().get_node("Node/Script/Board")
 onready var parent_node = int(get_parent().name)
 onready var label = get_tree().get_root().get_node("Node/UI/Info/Label")
 
+onready var your_pattern
+
+func _ready():
+#	print("status =",get_node("/root/global_network").my_network_status)
+#	if get_node("/root/global_network").my_network_status == 1:
+#		your_pattern = board.player.X
+#	elif get_node("/root/global_network").my_network_status == 2:
+#		your_pattern = board.player.O
+#	print("your pattern "+str(your_pattern))
+	pass
+
 func _on_TextureButton_pressed():
+	print("parent_node",parent_node)
+	print(get_node("../Sprite"))
 	rpc("play")
 	pass # replace with function body
 	
@@ -22,8 +35,8 @@ sync func play():
 		pass
 	else:
 		match board.player_currentTurn :
-			board.Player.X :
-				label.text = "Player X turn"
-			board.Player.O:
-				label.text = "Player O turn"
+			board.player.X :
+				label.text = "It is X's turn"
+			board.player.O :
+				label.text = "It is O's turn"
 	pass
