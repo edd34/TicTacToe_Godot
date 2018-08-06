6d02b67fb553289418a2d6ab41ca3eff1bd0664b
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-08-06 20:07:14 +0200

    update logic for remote play

--- Scripts/button/button_remote.gd
+++ Scripts/button/button_remote.gd
@@ -7,23 +7,27 @@ onready var label = get_tree().get_root().get_node("Node/UI/Info/Label")
 onready var your_pattern
 
 func _ready():
-#	print("status =",get_node("/root/global_network").my_network_status)
-#	if get_node("/root/global_network").my_network_status == 1:
-#		your_pattern = board.player.X
-#	elif get_node("/root/global_network").my_network_status == 2:
-#		your_pattern = board.player.O
-#	print("your pattern "+str(your_pattern))
+	print("status =",get_node("/root/global_network").my_network_status)
+	if get_node("/root/global_network").my_network_status == 1:
+		your_pattern = board.Player.X
+		label.text = "It's your turn, your are X"
+	elif get_node("/root/global_network").my_network_status == 2:
+		your_pattern = board.Player.O
+		label.text = "Wait for the opponent, your are O"
+	print("your pattern "+str(your_pattern))
 	pass
 
 func _on_TextureButton_pressed():
 	print("parent_node",parent_node)
 	print(get_node("../Sprite"))
-	rpc("play")
+	#if board.state != board.GameState.FINISHED:
+	if board.player_currentTurn == your_pattern:
+		rpc("play")
 	pass # replace with function body
 	
 sync func play():
-	if (board.state == board.GameState.IN_PROGRESS and \
-	!board.isCellValueAlreadySet(parent_node)):
+	if board.state == board.GameState.IN_PROGRESS and \
+	!board.isCellValueAlreadySet(parent_node):
 		if board.player_currentTurn == board.Player.X :
 			get_node("../Sprite").setSprite_X()
 		elif board.player_currentTurn == board.Player.O :
@@ -31,12 +35,31 @@ sync func play():
 		board.mark(parent_node)
 		
 	if(board.state == board.GameState.FINISHED):
-		label.text = "Winner is "+board.getWinner()+" !"
+		match board.player_currentTurn :
+			your_pattern :
+				label.text = "You won "+board.getWinner()+" !"
+			_ :
+				label.text = "You loose"
 		pass
 	else:
 		match board.player_currentTurn :
-			board.player.X :
-				label.text = "It is X's turn"
-			board.player.O :
-				label.text = "It is O's turn"
+			your_pattern :
+				label.text = "It's your turn"
+			_ :
+				label.text = "Wait for the opponent"
+			
+			
+		#match board.player_currentTurn :
+		#	your_pattern :
+		#		label.text = "It is your turn, you are "+str(your_pattern)
+		#	_ :
+		#		label.text = "Wait for the opponent"
+	pass
+	
+sync func disconnect_game():
+	get_tree().set_network_peer(null)
+	pass
+	
+sync func goto_main_menu():
+	get_node("/root/global").goto_scene("res://Scenes/MainMenu.tscn")
 	pass
