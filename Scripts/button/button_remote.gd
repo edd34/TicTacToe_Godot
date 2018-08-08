10055f0b191d49a7358ee01249c3876157be0dca
Eddine Farid OMAR <eddine.farid@hotmail.fr>
2018-08-09 01:16:38 +0200

    add draw mode and SFX

--- Scripts/button/button_remote.gd
+++ Scripts/button/button_remote.gd
@@ -3,16 +3,24 @@ extends TextureButton
 onready var board = get_tree().get_root().get_node("Node/Script/Board")
 onready var parent_node = int(get_parent().name)
 onready var label = get_tree().get_root().get_node("Node/UI/Info/Label")
+onready var PlayCross_node = get_tree().get_root().get_node("Node/SFX/PlayCross")
+onready var PlayCircle_node = get_tree().get_root().get_node("Node/SFX/PlayCircle")
+onready var PlayWin_node = get_tree().get_root().get_node("Node/SFX/Win")
+onready var PlayLose_node = get_tree().get_root().get_node("Node/SFX/Lose")
+onready var PlayDraw_node = get_tree().get_root().get_node("Node/SFX/Lose")
 
 onready var your_pattern
 
 func _ready():
+	print("node",PlayCross_node)
 	print("status =",get_node("/root/global_network").my_network_status)
 	if get_node("/root/global_network").my_network_status == 1:
 		your_pattern = board.Player.X
+		PlayCross_node.play()
 		label.text = "It's your turn, your are X"
 	elif get_node("/root/global_network").my_network_status == 2:
 		your_pattern = board.Player.O
+		PlayCircle_node.play()
 		label.text = "Wait for the opponent, your are O"
 	print("your pattern "+str(your_pattern))
 	pass
@@ -30,16 +38,25 @@ sync func play():
 	!board.isCellValueAlreadySet(parent_node):
 		if board.player_currentTurn == board.Player.X :
 			get_node("../Sprite").setSprite_X()
+			PlayCross_node.play()
 		elif board.player_currentTurn == board.Player.O :
 			get_node("../Sprite").setSprite_O()
+			PlayCircle_node.play()
 		board.mark(parent_node)
-		
-	if(board.state == board.GameState.FINISHED):
+	
+	if board.return_available_move().size() == 0 :
+		board.state = board.GameState.FINISHED
+		label.text = "Draw"
+		PlayDraw_node.play()
+		print("Draw")
+	elif board.state == board.GameState.FINISHED :
 		match board.player_currentTurn :
 			your_pattern :
 				label.text = "You won "+board.getWinner()+" !"
+				PlayWin_node.play()
 			_ :
 				label.text = "You loose"
+				PlayLose_node.play()
 		pass
 	else:
 		match board.player_currentTurn :
